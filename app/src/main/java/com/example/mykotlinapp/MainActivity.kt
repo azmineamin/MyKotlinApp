c1136ff321b54e4f03d6412d7dcd9fd9441f584e
Md. Azmine Amin Mormo <azmine.mormo@northsouth.edu>
2025-10-28 12:02:57 +0600

    this is 2nd commit for the test.

--- app/src/main/java/com/example/mykotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/mykotlinapp/MainActivity.kt
@@ -5,7 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+// This is a comment for the test commit
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
